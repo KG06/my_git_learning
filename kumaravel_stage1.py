331d89ad955d2c5bf63dfbb379f491aea6ac4901
kumaravel <gkumaravel06@gmail.com>
2026-02-26 00:09:30 +0530

    Stage 1 - Commit

---
+++ kumaravel_stage1.py
@@ -0,0 +1,21 @@
+
+
+def perform_action(num1, num2, action):
+    if action == '+':
+        return num1 + num2
+    elif action == '-':
+        return num1 - num2
+    elif action == '/':
+        return num1 / num2
+    elif action == '*':
+        return num1 * num2
+    else:
+        return "Action error!"
+
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+operand = str(input("Enter Operand: "))
+
+result = perform_action(number1, number2, '+')
+
+print(result)
